90d70b769a896cf064fcd801ce42f3a16d5b240e
Papa Meow <122598528+CJi88@users.noreply.github.com>
2024-03-09 21:34:26 +0800

    initial

---
+++ src/main/java/com/motorph/group2/Group2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.motorph.group2;
+
+/**
+ *
+ * @author enuj
+ */
+public class Group2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//        EmployeesGUI payGui = new EmployeesGUI();
+//        payGui.showGUI();
+    }
+}
